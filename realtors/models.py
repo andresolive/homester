d345d56b4c1d84d97af770b430414266e00340f2
Andrés Olivé <andresolive@gmail.com>
2022-10-31 12:47:16 -0400

    fixed realtors model

--- realtors/models.py
+++ realtors/models.py
@@ -9,7 +9,7 @@ class Realtor(models.Model):
   phone = models.CharField(max_length=20)
   email = models.CharField(max_length=20)
   is_mvp = models.BooleanField(default=False)
-  hire_date = models.Date(default=datetime.now, blank=True)
+  hire_date = models.DateField(default=datetime.now, blank=True)
 
   def __str__(self):
     return self.name
